c9314418a4c69e82677d0fe1608afcfe29108909
apk2sf <apk2sf@163.com>
2018-02-09 15:26:48 +0800

    添加添加用户的功能
    Co-Authored-By: apk2sf <apk2sf@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -15,35 +15,59 @@ Bmob.initialize("03fb6d9aad6aa01a12054dd823ff110b", "18d70d5ad717839d2a78d9d20bd
 //  });
 
 
-var Student = Bmob.Object.extend("Student")
-var stu = new Student()
+function addData() {
+    var username = $("#username").val();
+    var password = $("#password").val();
 
-stu.set("name","张三");
-stu.set("age",120);
+    var User = Bmob.Object.extend("User")
+    var user = new User();
 
-stu.save(null,{
-	success:function(object){
-		alert("success")
-	},
-	
-	error:function(model,error){
-		alert("error")
-	}
-	
-})
+    user.set("username", username)
+    user.set("password", password)
 
-var stu2 = new Student()
+    user.save(null, {
+        success: function (object) {
+            console.log("object:" + object);
+        },
+        error: function (model, error) {
+            console.log("model" + model);
+            console.log("error:" + error);
+        }
+    })
 
-stu2.set("name","张三2");
-stu2.set("age",1202);
 
-stu2.save(null,{
-	success:function(object){
-		alert("success")
-	},
-	error:function(model,error){
-		alert("error")
-	}
-	
-})
+}
+
+//
+// var Student = Bmob.Object.extend("Student")
+// var stu = new Student()
+//
+// stu.set("name", "张三");
+// stu.set("age", 120);
+//
+// stu.save(null, {
+//     success: function (object) {
+//         alert("success")
+//     },
+//
+//     error: function (model, error) {
+//         alert("error")
+//     }
+//
+// })
+//
+// var stu2 = new Student()
+//
+// stu2.set("name", "张三2");
+// stu2.set("age", 1202);
+//
+// stu2.save(null, {
+//     success: function (object) {
+//         alert("success")
+//     },
+//     error: function (model, error) {
+//         alert("error")
+//     }
+//
+// })
 
