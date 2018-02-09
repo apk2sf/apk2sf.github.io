6e3b8c066e556f61987635371e7c31497ce9f0d1
apk2sf <apk2sf@163.com>
2018-02-09 15:10:59 +0800

    apk2sf
    initproject
    
    Co-Authored-By: apk2sf <apk2sf@users.noreply.github.com>

---
+++ js/index.js
@@ -0,0 +1,49 @@
+//初始化bmob
+Bmob.initialize("03fb6d9aad6aa01a12054dd823ff110b", "18d70d5ad717839d2a78d9d20bd47678");
+
+
+// var GameScore = Bmob.Object.extend("GameScore");
+//  var gameScore = new GameScore();
+//  gameScore.set("score", 1337);
+//  gameScore.save(null, {
+//    success: function(object) {
+//      alert("create object success, object id:"+object.id);
+//    },
+//    error: function(model, error) {
+//      alert("create object fail");
+//    }
+//  });
+
+
+var Student = Bmob.Object.extend("Student")
+var stu = new Student()
+
+stu.set("name","张三");
+stu.set("age",120);
+
+stu.save(null,{
+	success:function(object){
+		alert("success")
+	},
+	
+	error:function(model,error){
+		alert("error")
+	}
+	
+})
+
+var stu2 = new Student()
+
+stu2.set("name","张三2");
+stu2.set("age",1202);
+
+stu2.save(null,{
+	success:function(object){
+		alert("success")
+	},
+	error:function(model,error){
+		alert("error")
+	}
+	
+})
+
